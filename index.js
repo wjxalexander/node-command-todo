76303d59445625d44e5043bb5efbc586db282bf6
jingxin wang <wjxalexander@hotmail.com>
2020-09-03 22:31:59 +0800

    publish

--- index.js
+++ index.js
@@ -15,110 +15,129 @@ async function clear(...tasks) {
     await db.write([])
 }
 
+async function askForAction(list,index){
+    inquirer.prompt({
+        type: "list",
+        name: "action",
+        message: "choose next step",
+        choices: [{
+                name: "quit",
+                value: "quit"
+            },
+            {
+                name: "done",
+                value: "markAsDone"
+            },
+            {
+                name: "todo",
+                value: "markAsUnDone"
+            },
+            {
+                name: "edit title",
+                value: "updateTitle"
+            },
+            {
+                name: "delete",
+                value: "remove"
+            }
+        ]
+    }).then(answers2 => {
+        const {
+            action
+        } = answers2
+        const chosenIdx = parseInt(index)
+        switch (action) {
+            case 'markAsDone':
+                list[chosenIdx].done = true
+                db.write(list)
+                break;
+            case 'markAsUnDone':
+                list[chosenIdx].done = false
+                db.write(list)
+                break;
+            case 'updateTitle':
+                inquirer.prompt({
+                    type: "input",
+                    name: "title",
+                    message: "what is your new title",
+                    default: list[chosenIdx].title 
+                }).then(ans=>{
+                    list[chosenIdx].title = ans.title
+                    db.write(list)
+                })
+                break;
+            case "delete":
+                list.splice(chosenIdx,1)
+                db.write(list)
+                break;
+            default:
+                break;
+        }
+    })
+}
+
+async function addTask(list){
+    inquirer.prompt({
+        type: "input",
+        name: "newtask",
+        message: "add a task",
+    }).then(ans=>{
+        list.push({title:ans.newtask, done:false})
+        db.write(list)
+    })
+}
+
+async function printTask(list){
+    inquirer
+    .prompt([
+        /* Pass your questions in here */
+        {
+            type: "list",
+            name: "index",
+            message: "请选择任务",
+            choices: [{
+                name: "quit",
+                value: "-1"
+            }, ...list.map(({
+                title,
+                done
+            }, index) => ({
+                name: `${done ? "[x]" : "[_]"}${index + 1} - ${title}`,
+                value: index.toString()
+            })), {
+                name: "+ add task",
+                value: "-2"
+            }]
+        },
+    ])
+    .then(answers => {
+        // Use user feedback for... whatever!!
+        const {
+            index
+        } = answers
+        switch (parseInt(index)) {
+            case -2:
+                await addTask(list)
+                break;
+            case -1:
+                break;
+            default:
+                await askForAction(list,index)
+                break;
+        }
+    })
+    .catch(error => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else when wrong
+        }
+    });
+}
+
 async function showAll() {
     const list = await db.read()
-    inquirer
-        .prompt([
-            /* Pass your questions in here */
-            {
-                type: "list",
-                name: "index",
-                message: "请选择任务",
-                choices: [{
-                    name: "quit",
-                    value: "-1"
-                }, ...list.map(({
-                    title,
-                    done
-                }, index) => ({
-                    name: `${done ? "[x]" : "[_]"}${index + 1} - ${title}`,
-                    value: index.toString()
-                })), {
-                    name: "+ add task",
-                    value: "-2"
-                }]
-            },
-        ])
-        .then(answers => {
-            // Use user feedback for... whatever!!
-            const {
-                index
-            } = answers
-            switch (parseInt(index)) {
-                case -2:
-                    break;
-                case -1:
-                    break;
-                default:
-                    inquirer.prompt({
-                        type: "list",
-                        name: "action",
-                        message: "choose next step",
-                        choices: [{
-                                name: "quit",
-                                value: "quit"
-                            },
-                            {
-                                name: "done",
-                                value: "markAsDone"
-                            },
-                            {
-                                name: "todo",
-                                value: "markAsUnDone"
-                            },
-                            {
-                                name: "edit title",
-                                value: "updateTitle"
-                            },
-                            {
-                                name: "delete",
-                                value: "remove"
-                            }
-                        ]
-                    }).then(answers2 => {
-                        
-                        const {
-                            action
-                        } = answers2
-                        const chosenIdx = parseInt(index)
-                        switch (action) {
-                            case 'markAsDone':
-                                list[chosenIdx].done = true
-                                db.write(list)
-                                break;
-                            case 'markAsUnDone':
-                                list[chosenIdx].done = false
-                                db.write(list)
-                                break;
-                            case 'updateTitle':
-                                inquirer.prompt({
-                                    type: "input",
-                                    name: "title",
-                                    message: "what is your new title",
-                                    default: list[chosenIdx].title 
-                                }).then(ans=>{
-                                    list[chosenIdx].title = ans.title
-                                    db.write(list)
-                                })
-                                break;
-                            case "delete":
-                                list.splice(chosenIdx,1)
-                                db.write(list)
-                                break;
-                            default:
-                                break;
-                        }
-                    })
-                    break;
-            }
-        })
-        .catch(error => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else when wrong
-            }
-        });
+    await printTask(list)
 }
 
 
